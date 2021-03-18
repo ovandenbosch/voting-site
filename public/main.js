ff79b482779d6eb36e1eb87a2ac647b1cd4e016e
Oliver van den Bosch <vanders06@icloud.com>
2021-03-18 05:29:02 -0400

    Updated if voted

--- public/main.js
+++ public/main.js
@@ -1,31 +1,34 @@
 const form = document.getElementById("vote-form");
-const votechart = document.getElementById("titleandchart")
-var event
+const votechart = document.getElementById("titleandchart");
+const rad_buttons = document.getElementsByName("candidate");
+const error_message = document.getElementById("errormessage");
+const hasvoted = window.localStorage.getItem("voted");
+var event;
 
 // Hide chart until submission
 function Showchart() {
-  votechart.style.display = "block"
-  window.localStorage.setItem('voted', true)
+  votechart.style.display = "block";
+  window.localStorage.setItem("voted", true);
 }
 
-const hasvoted = window.localStorage.getItem('voted')
-
-if (window.localStorage.getItem('voted') == "true") {
-  votechart.style.display = "block"
-  button.className = "btn disabled"
-  alert("You have already voted!")
-
+// Checking if someone has already voted
+if (window.localStorage.getItem("voted") == "true") {
+  votechart.style.display = "block";
+  button.className = "btn disabled";
+  // Alert message
+  alert("You have already voted!");
+  error_message.innerHTML = "You have already voted";
+  // Disables all buttons
+  rad_buttons.forEach((rad_button) => {
+    rad_button.setAttribute("disabled", "disabled");
+  });
 }
-
-
 
 // Form submit event
 form.addEventListener("submit", (e) => {
-  
-  
   const choice = document.querySelector("input[name=candidate]:checked").value;
   const data = { candidate: choice };
-
+  // What happens if there is a vote
   fetch("http://localhost:3000/vote", {
     method: "post",
     body: JSON.stringify(data),
@@ -39,6 +42,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
+// What happens if there is a vote that needs to be counted
 fetch("http://localhost:3000/vote")
   .then((res) => res.json())
   .then((data) => {
@@ -72,6 +76,7 @@ fetch("http://localhost:3000/vote")
       { label: "Candidate C", y: voteCounts.CandidateC, id: "CandidateC" },
     ];
 
+    // Auto-update title
     const chartContainer = document.querySelector("#chartContainer");
 
     if (chartContainer) {
@@ -82,6 +87,7 @@ fetch("http://localhost:3000/vote")
         ).textContent = `Total Votes: ${e.detail.totalVotes}`;
       });
 
+      // Canvas JS chart creation
       const chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         theme: "theme1",
@@ -97,8 +103,11 @@ fetch("http://localhost:3000/vote")
       });
       chart.render();
 
+      ///////////////////////
+      // Pusher integration//
+      ///////////////////////
       // Enable pusher logging - don't include this in production
-      Pusher.logToConsole = true;
+      Pusher.logToConsole = false;
 
       var pusher = new Pusher("c05df0740880d4b1eee9", {
         cluster: "us2",
@@ -108,15 +117,15 @@ fetch("http://localhost:3000/vote")
 
       channel.bind("candidate-vote", function (data) {
         dataPointsreal.forEach((point) => {
-          if (point.id == data.candidate) 
-          {
+          if (point.id == data.candidate) {
             point.y += data.points;
             totalVotes += data.points;
-            event = new CustomEvent('votesAdded',{detail:{totalVotes:totalVotes}});
+            event = new CustomEvent("votesAdded", {
+              detail: { totalVotes: totalVotes },
+            });
             // Dispatch
-            document.dispatchEvent(event)
-            }
-          
+            document.dispatchEvent(event);
+          }
         });
         chart.render();
       });
